3ec69f57eb745078276aa77159121d623fba939d
carmensoto <carmensoto410@gmail.com>
2021-11-09 14:26:06 -0300

    conDB

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -1,7 +1,12 @@
 <?php 
-$conn= new mysqli('localhost', 'root','root','prueba_gdlwebcamp',3307);
+/*$conn= new mysqli('localhost', 'root','root','prueba_gdlwebcamp',3307);
+if($conn->connect_errno){
+    echo $error -> $conn->connect_errno;
+}*/
+
+
+$conn= new mysqli('remotemysql.com', 'gSMAo2fsHd','UbYqwskT16','gSMAo2fsHd',3306);
 if($conn->connect_errno){
     echo $error -> $conn->connect_errno;
 }
-
 ?>
