685a9affa111795c877c278c2d2b5c40c7ca8dd6
carmencitasoto4 <90402144+carmencitasoto4@users.noreply.github.com>
2022-01-14 18:55:13 -0300

    Update bd_conexion.php

--- includes/funciones/bd_conexion.php
+++ includes/funciones/bd_conexion.php
@@ -5,7 +5,7 @@ if($conn->connect_errno){
 }*/
 
 
-$conn= new mysqli('remotemysql.com', 'gSMAo2fsHd','UbYqwskT16','gSMAo2fsHd',3306);
+$conn= new mysqli('us-cdbr-east-05.cleardb.net', 'b953cd2e7d885c','935445dd','heroku_04372a61cd00d42',3306);
 if($conn->connect_errno){
     echo $error -> $conn->connect_errno;
 }
